aeb2670b9ef0e41f670ca1555d64d856195b0d57
HeFung <HeFung@users.noreply.github.com>
2024-08-15 09:04:03 +0800

    deploy: viptv-work/dev@e62d9fde8875bda905cb5622ab250ffec15ad34b

--- assets/vidstack-C-clE4br-Dvc7nHMD.js
+++
@@ -1,1 +0,0 @@
-import{bo as i,aX as s}from"./app-D1y_3dFn.js";class r{constructor(a){this.La=a}Xa(){i(this.ya)&&this.fg()}$(){s(this.ya)&&window.cancelAnimationFrame(this.ya),this.ya=void 0}fg(){this.ya=window.requestAnimationFrame(()=>{i(this.ya)||(this.La(),this.fg())})}}export{r as R};
